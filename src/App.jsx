f7668a61393c533c2c62cb5265d9671e3f8f1dc0
Daffa Budi Prasetya <daffa.budi2003@gmail.com>
2024-12-19 05:20:58 +0700

    tanpa atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ import Button from "./components/Elements/Buttons/index"
 function App() {
 
   return (
-   <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-    <div className="flex gap-x-3">
-      <Button variant="bg-red-700">Login</Button>
-      <Button variant="bg-slate-700">Logout</Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+   <div className="flex justify-center min-h-screen items-center">
+    <div className="w-full max-w-xs">
+      <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+      <p className="font-medium text-slate-500 mb-8">Welcome, Please Enter Your Detail</p>
+      <form action="">
+        <div className="mb-6">
+          <label htmlFor="" className="block text-slate-700 text-sm font-boldmb-2">Email</label>
+          <input type="text" className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="example@email.com" />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="" className="block text-slate-700 text-sm font-boldmb-2">Password</label>
+          <input type="password" className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="******" />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="" className="block text-slate-700 text-sm font-boldmb-2">Password</label>
+          <input type="password" className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="******" />
+        </div>
+        <Button classname="bg-blue-600 w-full">Login</Button>
+      </form>
     </div>
    </div>
   )
