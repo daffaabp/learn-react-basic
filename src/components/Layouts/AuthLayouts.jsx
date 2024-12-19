bb0cce74f2bc07db65d4357abfe84d42005ba714
Daffa Budi Prasetya <daffa.budi2003@gmail.com>
2024-12-19 10:55:35 +0700

    ternary operator

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const AuthLayouts = (props) => {
-  const { children, title } = props;
+  const { children, title, type } = props;
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-xs">
         <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
         <p className="font-medium text-slate-500 mb-8">Welcome, Please Enter Your Detail</p>
+
         {children}
+
+        {/* Ternary operator sebaiknya digunakan jika kondisinya hanya dua */}
+        <p className='text-sm mt-5'>
+          {type === "login"
+            ? "Don't have an account? "
+            : "Have an account? "}
+
+          {type === "login" && (
+            <Link to="/register" className='font-bold text-blue-600'>Register</Link>
+          )}
+
+          {type === "register" && (
+            <Link to="/login" className='font-bold text-blue-600'>Login</Link>
+          )}
+        </p>
       </div>
     </div>
   )
 }
+
 
 export default AuthLayouts
